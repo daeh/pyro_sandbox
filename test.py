552ef4b78f1bf0426c9f33acf8fe76d69be716b0
Dae Houlihan <daeda@mit.edu>
2018-11-19 04:28:32 -0500

    test

---
+++ test.py
@@ -0,0 +1,36 @@
+%matplotlib inline
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import os
+
+import torch
+torch.set_default_dtype(torch.float64)  # double precision for numerical stability
+
+import collections
+import argparse
+import matplotlib.pyplot as plt
+
+import pyro
+import pyro.distributions as dist
+import pyro.poutine as poutine
+from pyro.optim import Adam
+from pyro.infer import SVI, Trace_ELBO
+from torch.distributions import constraints
+import torch.nn.functional as F
+from torch.autograd import Variable
+
+# search_inference.py
+# from search_inference import factor, HashingMarginal, memoize, Search
+
+pyro.enable_validation(True)
+
+# defining sigmoid, logistic functions that work on torch tensors
+def sigmoid(x):
+    return logistic(x, maxVal=1.0, steepness=1.0, midpoint=0.0)
+
+def logistic(x, maxVal=1.0, steepness=1.0, midpoint=0.0):
+    # https://en.wikipedia.org/wiki/Logistic_function
+    return torch.tensor(maxVal)/(torch.tensor(1.)+torch.exp(-steepness*(x-midpoint)))
+
+myPlotColors = ["olive", "green", "gold", "darkgrey", "red", "maroon", "darkorchid", "midnightblue"]
